b7b1ba451314aee8991e5d8c9161d8b64c45146b
sashadyg <90382567+sashadyg@users.noreply.github.com>
2022-02-20 19:48:25 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -6,10 +6,34 @@ const messageRef = document.querySelector('.feedback-form textarea')
 const submitRef = document.querySelector('.feedback-form button')
 
 formRef.addEventListener('input', throttle(onTextareaInput, 500))
+formRef.addEventListener('submit', submitAction)
+
+checkTextarea()
 
 function onTextareaInput(e) {
     const message = messageRef.value;
     const email = emailRef.value
 
     localStorage.setItem("feedback-form-state", JSON.stringify({ email, message }))
+}
+
+function checkTextarea() {
+    const savedMessage = localStorage.getItem("feedback-form-state")
+    const savedParsedMessage = JSON.parse(savedMessage)
+    
+    if (savedMessage) {
+        emailRef.value = savedParsedMessage.email
+        messageRef.value = savedParsedMessage.message   
+    }
+}
+
+function submitAction(e) {
+    e.preventDefault()
+
+    const savedMessage = localStorage.getItem("feedback-form-state")
+    const savedParsedMessage = JSON.parse(savedMessage)
+    
+    console.log(savedParsedMessage);
+    e.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
 }
